16645652d516a276770303aecf66bc5674f6e169
Michael <michaelodell660@gmail.com>
2021-11-03 18:48:28 -0700

    changed bonds on all classes

--- character_classes.py
+++ character_classes.py
@@ -13,44 +13,44 @@ barbarian = {
 bard = {
     'damage': 'd6',
     'bonds' : [
-        "This is not my first adventure with _____.",
-        "I sang stories of _____ long before i ever met them in person.",
-        "_____ is often the butt of my jokes.",
-        "I am writting a ballad about the adventures of _____."
+        "This is not my first adventure with character-name.",
+        "I sang stories of character-name long before i ever met them in person.",
+        "character-name is often the butt of my jokes.",
+        "I am writting a ballad about the adventures of character-name."
     ],
 }
 cleric = {
     'damage': 'd6',
     'bonds' : [
-        "_____ has insluted my deity; I do ot trust them.",
-        "_____ is a good and faithful person; I trust them implicity",
-        "_____ is in constant danger, I will keep them safe.",
-        "I am working on converting _____ to my faith."
+        "character-name has insluted my deity; I do ot trust them.",
+        "character-name is a good and faithful person; I trust them implicity",
+        "character-name is in constant danger, I will keep them safe.",
+        "I am working on converting character-name to my faith."
     ],
 }
 druid = {
     'damage': 'd6',
     'bonds': [
-        "_____ smells more like prey than a hunter.",
-        "The spirits spoke to me of a great danger that follows _____.",
-        "I have shown _____ a secret rite of the land.",
-        "_____ has tasted my blood and I theirs. We are bound by it."
+        "character-name smells more like prey than a hunter.",
+        "The spirits spoke to me of a great danger that follows character-name.",
+        "I have shown character-name a secret rite of the land.",
+        "character-name has tasted my blood and I theirs. We are bound by it."
     ],
 }
 fighter = {
     'damage': 'd10',
     'bonds': [
-        "_____ owes me their life, whether they admit it or not.",
-        "I have sworn to protect _____",
-        "I worry about the ability of _____ to survice in the dungeon",
-        "_____ is soft, but I will make them hard like me"
+        "character-name owes me their life, whether they admit it or not.",
+        "I have sworn to protect character-name",
+        "I worry about the ability of character-name to survice in the dungeon",
+        "character-name is soft, but I will make them hard like me"
     ]
 }
 immolator = {
     'damage': 'd8',
     'bonds': [
-        "_____ has felt the hellish touch of fire, now they know my strength.",
-        "I will teach _____ the true meaning of sacrifice.",
-        "I cast somthing into the fire for _____ and still owe them their due."
+        "character-name has felt the hellish touch of fire, now they know my strength.",
+        "I will teach character-name the true meaning of sacrifice.",
+        "I cast somthing into the fire for character-name and still owe them their due."
     ]
 }
